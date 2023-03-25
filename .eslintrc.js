90ca89d7e933cf1926de26144ca5f147e212ec32
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-03-25 08:30:14 +0300

    chore: startup template

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "plugin:unicorn/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    ignorePatterns: ["packages/**/dist"],
+    rules: {},
+};
