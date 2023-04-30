802a2785d1c494b92e238be5ddd99d28c2f3badc
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-04-30 10:21:43 +0300

    add transformCJS function

---
+++ src/utils/code/cjs_to_esm.spec.ts
@@ -0,0 +1,99 @@
+import Ajv from "ajv";
+import addFormat from "ajv-formats";
+import standaloneCode from "ajv/dist/standalone";
+import path from "node:path";
+import { createServer, Plugin } from "vite";
+import { expect, suite, test } from "vitest";
+import { transformCJS } from "./cjs_to_esm";
+
+const example_schema = {
+    $id: "id",
+    $async: true, // will result in importing "ajv/dist/runtime/validation_error.js"
+    type: "object",
+    properties: {
+        nested: {
+            type: "number",
+            minimum: 2,
+        },
+        email: {
+            type: "string",
+            format: "email",
+        },
+        test: { type: "string", format: "uuid" },
+        foo: { type: "string", format: "date-time" },
+        time: { type: "string", format: "time" },
+        binary: { type: "string", format: "binary" },
+        url: { type: "string", format: "url" },
+        double: { type: "string", format: "double" },
+    },
+};
+
+const valid_import_id = "valid_import";
+const invalid_import_id = "invalid_import";
+
+function createAjvInstance() {
+    const ajv = new Ajv({
+        code: {
+            source: true,
+            esm: true,
+        },
+        schemas: [example_schema],
+    });
+    addFormat(ajv);
+    return ajv;
+}
+
+async function createAjvViteInstance() {
+    const ajv = createAjvInstance();
+
+    const testPlugin: Plugin = {
+        name: "test",
+        async load(id) {
+            if (id == valid_import_id || id == invalid_import_id) {
+                const code = standaloneCode(ajv, ajv.getSchema("id"));
+
+                if (id == valid_import_id) {
+                    return transformCJS(code);
+                }
+
+                return code;
+            }
+        },
+    };
+
+    const vite = await createServer({
+        configFile: false,
+        root: path.resolve(__dirname, ".."),
+        plugins: [testPlugin],
+    });
+    return { ajv, vite };
+}
+
+suite("transform ajv compiled code from commonjs to esm", async () => {
+    test("transform match snapshot", async () => {
+        const ajv = createAjvInstance();
+
+        const schema = ajv.getSchema("id");
+        const code = transformCJS(standaloneCode(ajv, schema));
+        expect(code).toMatchSnapshot(code);
+    });
+
+    test("transformed code should work correctly with vite esm imports", async () => {
+        const { vite } = await createAjvViteInstance();
+        const m = await vite.ssrLoadModule(valid_import_id);
+
+        expect(
+            m.default,
+            "default export should be the validatation function"
+        ).toBeTypeOf("function");
+
+        expect(m.validate, "validate export should be a function").toBeTypeOf("function");
+        await expect(m.validate({ email: "w" })).rejects.toThrow(/validation failed/);
+    });
+
+    test("untransformed code should throw error", async () => {
+        const { vite } = await createAjvViteInstance();
+        const m = vite.ssrLoadModule(invalid_import_id);
+        await expect(m).rejects.toThrow(/require is not defined/);
+    });
+});
---
+++ src/utils/code/cjs_to_esm.ts
@@ -0,0 +1,39 @@
+// ajv currently does not support esm import for some imports
+// the code expected to work with ajv compiled code and does not handle every cjs cases
+export function transformCJS(code: string) {
+	const paths: Record<string, number> = {};
+
+	let prepend = "";
+	let import_n = 0;
+
+	code = code.replace(
+		/const (\w+) = require\("([^"]*)"\)((\.[^.;]+)+);/g,
+		(s, fn, path, imports) => {
+			const [, first] = imports.split(".");
+
+			const m = first.match(/(\w+)(\["[^"]+"\])*/);
+			if (!m) return "";
+
+			const first_import = m[1];
+			let property_access = m[2];
+
+			if (!property_access) {
+				property_access = imports.slice(first_import.length + 1);
+			}
+
+			if (!paths[path]) {
+				let imported = `import_${import_n}`;
+				if (first_import != "default") {
+					imported = `{${first_import} as ${imported}}`;
+				}
+				prepend += `import ${imported} from "${path}.js";\n`;
+				paths[path] = import_n;
+				import_n++;
+			}
+
+			const alias = `import_${paths[path]}${property_access}`;
+			return `const ${fn} = ${alias};`;
+		}
+	);
+	return prepend + code.replace('"use strict";', "");
+}
