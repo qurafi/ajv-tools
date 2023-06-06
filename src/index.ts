039b926dc2f9604df68376bf0c9dcd0958809461
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2023-06-06 19:51:21 +0300

    feat: add some misc imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./core/index.js";
-export { default as unpluginAjvTools } from "./unplugin/index.js";
+export { default as unpluginAjvTools, PluginOptions } from "./unplugin/index.js";
+export { resolveSchemaRef } from "./core/ajv.js";
